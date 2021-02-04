aec8fd277af75ee3c819e890279d61c6177944d0
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2021-02-03 19:20:05 -0800

    Update FindBall.java

--- src/main/java/frc/robot/FindBall.java
+++ src/main/java/frc/robot/FindBall.java
@@ -178,7 +178,7 @@ public class FindBall {
 		Mat blurred = new Mat(), hsv = new Mat(mat.rows(), mat.cols(), CvType.CV_8UC2), mask = new Mat();
 		Imgproc.GaussianBlur(mat, blurred, new Size(11, 11), 0);
 		Imgproc.cvtColor(blurred, hsv, Imgproc.COLOR_BGR2HSV);
-		Core.inRange(hsv, new Scalar(20, 100, 50), new Scalar(30, 255, 255), mask);
+		Core.inRange(hsv, new Scalar(0, 150, 51), new Scalar(60, 255, 255), mask);
 		Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
 		Imgproc.erode(mask, mask, element, new Point(), 2);
 		Imgproc.dilate(mask, mask, element, new Point(), 2);
